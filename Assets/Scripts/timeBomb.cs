c50741be12dc3193b6dc5af97130fa60d90fecca
Nick2046L <459208185nick@gmail.com>
2021-05-03 20:18:22 +1000

    timebomb

--- Assets/Scripts/timeBomb.cs
+++ Assets/Scripts/timeBomb.cs
@@ -1,8 +1,32 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class timeBomb : MonoBehaviour
 {
-    
+    [SerializeField] float explosionRange;
+    [SerializeField] float timeToBlow;
+    [SerializeField] float explosionPower;
+
+    void Start()
+    {
+        Invoke("Explode", timeToBlow);
+    }
+    void Explode()
+    {
+        Collider[] hitObjects = Physics.OverlapSphere(transform.position, explosionRange);
+
+        foreach (Collider hitItem in hitObjects)
+        {
+            
+           // Rigidbody rb = hitItem.GetComponent<Rigidbody>();
+
+           // if (rb != null)
+             //   rb.AddExplosionForce(explosionPower, transform.position, 3.0f);
+            
+
+            hitItem.attachedRigidbody?.AddExplosionForce(explosionPower, transform.position, explosionRange, 3.0f);
+        }
+        Destroy(gameObject);
+    }
 }
