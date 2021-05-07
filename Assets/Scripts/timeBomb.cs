f681ccec9579bdc31964e2b35470e85d75a77185
DanielRHolmesglen <78955613+DanielRHolmesglen@users.noreply.github.com>
2021-05-07 11:17:23 +1000

    Set up Shader scene
    Added text mesh pro
    added shader graph
    set up scene
    created button script to change shader

--- Assets/Scripts/timeBomb.cs
+++ Assets/Scripts/timeBomb.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class timeBomb : MonoBehaviour
 {
-    
+    [SerializeField] float explosionRange;
+    [SerializeField] float timeToBlow;
+    [SerializeField] float explosionPower;
+
+    void Start()
+    {
+        Invoke("Explode", timeToBlow);
+    }
+
+    void Explode()
+    {
+        Collider[] hitObjects = Physics.OverlapSphere(transform.position, explosionRange);
+
+        foreach (Collider hitItem in hitObjects)
+        {
+           hitItem.attachedRigidbody?.AddExplosionForce(explosionPower, transform.position, explosionRange, 3.0f);
+        }
+        Destroy(gameObject);
+        
+    }
+
 }
