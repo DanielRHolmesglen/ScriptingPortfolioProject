1cb4d5d77a63275ec84d3fe350cbc27449021e82
Vampire-blue <68678020+Vampire-blue@users.noreply.github.com>
2021-05-10 16:56:10 +1000

    colour and disolve

--- Assets/Scripts/ShaderInteractions.cs
+++ Assets/Scripts/ShaderInteractions.cs
@@ -6,18 +6,43 @@ using UnityEngine;
 /// </summary>
 public class ShaderInteractions : MonoBehaviour
 {
-    
+    [SerializeField] Renderer r;
+    [SerializeField] bool CROneRunning, CRTwoRunning;
+
     public void ShaderInteractionOne()
     {
+        r.material.SetColor("Color_A105B5F", Random.ColorHSV());
         Debug.Log("interaction one");
     }
     public void ShaderInteractionTwo()
     {
+        if (!CROneRunning && !CRTwoRunning) StartCoroutine(Dissolve());
         Debug.Log("interaction two");
     }
     public void ShaderInteractionThree()
     {
+        if (!CRTwoRunning && !CROneRunning) StartCoroutine(Solve());
         Debug.Log("interaction three");
     }
 
+    IEnumerator Dissolve()
+    {
+        CROneRunning = true;
+        while (r.material.GetFloat("_dis_Rate") < 1)
+        {
+            r.material.SetFloat("_dis_Rate", r.material.GetFloat("_dis_Rate") + 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+        CROneRunning = false;
+    }
+    IEnumerator Solve()
+    {
+        CRTwoRunning = true;
+        while (r.material.GetFloat("_dis_Rate") > 0)
+        {
+            r.material.SetFloat("_dis_Rate", r.material.GetFloat("_dis_Rate") - 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+        CRTwoRunning = false;
+    }
 }
