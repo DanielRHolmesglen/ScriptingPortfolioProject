f7d4441f74a8f0a062a2051657bf58d53b8e2ccb
WilliamC2020 <68672479+WilliamC2020@users.noreply.github.com>
2021-05-10 14:29:58 +1000

    commit

--- Assets/Scripts/timeBomb.cs
+++ Assets/Scripts/timeBomb.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class timeBomb : MonoBehaviour
 {
-    
+    [SerializeField] float explosionRange;
+    [SerializeField] float timeToBlow;
+    [SerializeField] float explosionPower;
+
+    void Start()
+    {
+        Invoke("Explode", timeToBlow);    
+    }
+
+    void Explode()
+    {
+        Collider[] hitObjects = Physics.OverlapSphere(transform.position, explosionRange);
+      
+        foreach (Collider hitItem in hitObjects)
+        {
+            Rigidbody rb = hitItem.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {//i feel using brackets is more readable
+               rb.AddExplosionForce(explosionPower, transform.position, explosionRange, 3.0f);
+            }
+           
+        }
+        Destroy(gameObject);
+
+
+    }
 }
