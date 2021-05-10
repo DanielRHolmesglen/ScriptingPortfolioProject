5ca6b0a1e4f9dcfcd5094b7a6beb9d56cbbbbcd0
spoocemoose <chloejfitchett@gmail.com>
2021-05-10 17:14:06 +1000

    Shaders Scripting
    Done shaders scripts

--- Assets/Scripts/ShaderInteractions.cs
+++ Assets/Scripts/ShaderInteractions.cs
@@ -6,18 +6,49 @@ using UnityEngine;
 /// </summary>
 public class ShaderInteractions : MonoBehaviour
 {
-    
+    [SerializeField] Renderer r;
+    [SerializeField] bool CROneRunning, CRTwoRunning;
+
     public void ShaderInteractionOne()
     {
+        r.material.SetColor("_BaseColour", Random.ColorHSV());
         Debug.Log("interaction one");
     }
     public void ShaderInteractionTwo()
     {
+        if (!CROneRunning) StartCoroutine(Dissolve());
         Debug.Log("interaction two");
     }
     public void ShaderInteractionThree()
     {
+        if (!CROneRunning) StartCoroutine(Reappear());
         Debug.Log("interaction three");
     }
 
+    IEnumerator Dissolve()
+    {
+        CROneRunning = true;
+
+        while (r.material.GetFloat("_DissolveRate") < 1)
+        {
+            r.material.SetFloat("_DissolveRate", r.material.GetFloat("_DissolveRate") + 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        CROneRunning = false;
+    }
+    IEnumerator Reappear()
+    {
+        CROneRunning = true;
+
+        while (r.material.GetFloat("_DissolveRate") > 0)
+        {
+            r.material.SetFloat("_DissolveRate", r.material.GetFloat("_DissolveRate") - 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        CRTwoRunning = false;
+    }
+
 }
+
