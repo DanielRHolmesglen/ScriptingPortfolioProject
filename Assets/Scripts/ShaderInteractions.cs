d7ea9fc3d0abe5e38eb3a63e427d89c923a6a382
Nick2046L <459208185nick@gmail.com>
2021-05-17 15:10:13 +1000

    AI

--- Assets/Scripts/ShaderInteractions.cs
+++ Assets/Scripts/ShaderInteractions.cs
@@ -1,23 +1,50 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 /// <summary>
 /// This script controls the shader using UI buttons
 /// </summary>
 public class ShaderInteractions : MonoBehaviour
 {
-    
+    [SerializeField] Renderer r;
+    [SerializeField] bool CROneRunning, CRTwoRunning;
     public void ShaderInteractionOne()
     {
+        r.material.SetColor("Color_F9B4FD53", Random.ColorHSV());
         Debug.Log("interaction one");
     }
     public void ShaderInteractionTwo()
     {
+        if (!CROneRunning && !CRTwoRunning) StartCoroutine(Dissolve());
         Debug.Log("interaction two");
     }
     public void ShaderInteractionThree()
     {
+        if (!CRTwoRunning && !CROneRunning) StartCoroutine(Reappear());
         Debug.Log("interaction three");
     }
 
+    IEnumerator Dissolve()
+    {
+        CROneRunning = true;
+        while (r.material.GetFloat("_Dissolve_Rate") < 1)
+        {
+            r.material.SetFloat("_Dissolve_Rate", r.material.GetFloat("_Dissolve_Rate") + 0.01f);
+                yield return new WaitForEndOfFrame();
+        }
+
+        CROneRunning = false;
+    }
+    IEnumerator Reappear()
+    {
+        CRTwoRunning = true;
+        while (r.material.GetFloat("_Dissolve_Rate") >0)
+        {
+            r.material.SetFloat("_Dissolve_Rate", r.material.GetFloat("_Dissolve_Rate") - 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        CRTwoRunning = false;
+    }
 }
