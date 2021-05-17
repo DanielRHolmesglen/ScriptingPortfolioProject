686b1a76af73392d4ddcacf98e900969937f92ff
WilliamC2020 <68672479+WilliamC2020@users.noreply.github.com>
2021-05-17 14:54:15 +1000

    www

--- Assets/Scripts/ShaderInteractions.cs
+++ Assets/Scripts/ShaderInteractions.cs
@@ -6,18 +6,63 @@ using UnityEngine;
 /// </summary>
 public class ShaderInteractions : MonoBehaviour
 {
-    
+    [SerializeField] Renderer r;
+    [SerializeField] bool CROneRunning, CRTwoRunning;
+
+
+
     public void ShaderInteractionOne()
     {
+        r.material.SetColor("_Base_Color", Random.ColorHSV());
         Debug.Log("interaction one");
     }
     public void ShaderInteractionTwo()
     {
+        if (CROneRunning != true)
+        {
+            StartCoroutine(Dissolve());
+        }
         Debug.Log("interaction two");
     }
     public void ShaderInteractionThree()
     {
+        if (CRTwoRunning != true)
+        {
+            StartCoroutine(Reappear());
+        }
         Debug.Log("interaction three");
+    }
+
+
+    public IEnumerator Dissolve()
+    {
+        CROneRunning = true;
+
+        if (CRTwoRunning == true)
+        {
+            StopCoroutine(Reappear());
+                
+        }
+
+        while (r.material.GetFloat("_Dissolve_Rate") < 1)
+        {
+            r.material.SetFloat("_Dissolve_Rate", r.material.GetFloat("_Dissolve_Rate") + 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        CROneRunning = false;
+    }
+    public IEnumerator Reappear()
+    {
+        CRTwoRunning = true;
+
+        while (r.material.GetFloat("_Dissolve_Rate") > 0)
+        {
+            r.material.SetFloat("_Dissolve_Rate", r.material.GetFloat("_Dissolve_Rate") - 0.01f);
+            yield return new WaitForEndOfFrame();
+        }
+
+        CRTwoRunning = false;
     }
 
 }
