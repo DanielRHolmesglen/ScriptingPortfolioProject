0650540be29b150a1e417f4561b5918eda13a65e
Gareth <gmorgan27@student.holmesglen.edu.au>
2021-06-01 10:57:03 +1000

    Add bad shader and simple control script

---
+++ Assets/Scripts/ShaderAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderAnimation : MonoBehaviour
+{
+    private Material _material;
+    private int _rotationTag;
+    private int _colorTag;
+    
+    private void Start()
+    {
+        _material = GetComponent<MeshRenderer>().material;
+        _rotationTag = Shader.PropertyToID("_CoolRotation");
+        _colorTag = Shader.PropertyToID("_CoolColor");
+    }
+
+    private void Update()
+    {
+        _material.SetFloat(_rotationTag, _material.GetFloat(_rotationTag) + Time.deltaTime);
+        // Outvars are disgusting. I'm not sure why this can't just return a tuple or something "normal"
+        Color.RGBToHSV(_material.GetColor(_colorTag), out var h, out var s, out var v);
+        _material.SetColor(_colorTag, Color.HSVToRGB(Mathf.Repeat(h + Time.deltaTime / 4f, 1f), s, v));
+    }
+}
